12b5be2bc80d36105477a5869d6b2bbcef942a4b
Marcos Vinycius Soares de Sousa <marcosvinyciusss@gmail.com>
2019-10-30 15:26:38 -0300

    Adicionando Structs

--- estruturas.c
+++ estruturas.c
@@ -1,14 +1,66 @@
 #include <stdio.h>
 
+typedef struct Brasileiro
+{
+    int cpf;
+    int rg;
+} brasileiro;
+
+typedef struct Estrangeiro
+{
+    char passaporte[10];
+    char pais_orig[20];
+} estrangeiro;
+
+typedef struct PNE
+{
+    int laudo;
+} pne;
+
+typedef struct Aluno
+{
+    int matricula;
+} aluno;
+
+typedef struct Professor
+{
+    int pis;
+} professor;
+
+typedef struct Cadastro
+{
+    char nome[15];
+    char sobrenome[30];
+    int nacionalidade;
+    union {
+        brasileiro brasileiro;
+        estrangeiro estrangeiro;
+    };
+    int tipo;
+    union {
+        aluno aluno;
+        professor professor;
+    };
+    int pnet;
+    union {
+        pne pne;
+    };
+    int dia;
+    int mes;
+    int ano;
+    int cep;
+    char endereco[200];
+} cadastros;
+
 typedef struct Disciplina
 {
     int codigo;
     char nome[30];
     int carga;
 
-}Disciplina;
+} disciplina;
 
 typedef struct Escola
 {
-    Disciplina disciplinas[10];
-}Escola;
+    disciplina disciplinas[10];
+} escola;
