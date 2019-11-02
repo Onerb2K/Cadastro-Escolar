d59898258dca1c9959126f861c6602cd1ff59ceb
Marcos Vinycius Soares de Sousa <marcosvinyciusss@gmail.com>
2019-11-01 23:21:21 -0300

    Update disciplinas.c

--- disciplinas.c
+++ disciplinas.c
@@ -40,7 +40,7 @@ int cadastraDisciplina(cadastros *Professores, disciplina *Disciplinas, int qtdD
         }
         else
         {
-            printf("\nO Professor não existe.\n");
+            printf("\nProfessor nao encontrado.\n");
             return 0;
         }
     }
@@ -49,11 +49,56 @@ int cadastraDisciplina(cadastros *Professores, disciplina *Disciplinas, int qtdD
         printf("\nNumero maximo de disciplinas atingido.\nNao e possivel inserir mais disciplinas.\n");
         return 0;
     }
-    
 };
 
-void alteraDisciplina(){
-
+void alteraDisciplina(cadastros *Professores, disciplina *Disciplinas)
+{
+    int codigo = 0;
+    int pis = 0;
+    int achou = 0;
+    int achou2 = 0;
+    int id_disciplina = 0;
+    int id_professor = 0;
+    scanf("%i", &codigo);
+    for (int i = 0; i <= sizeof(Disciplinas) / sizeof(Disciplinas[0]); i++)
+    {
+        if (Disciplinas[i].codigo == codigo)
+        {
+            achou = 1;
+            id_disciplina = i;
+            break;
+        }
+    }
+    if (achou == 1)
+    {
+        scanf("%i", &pis);
+        for (int i = 0; i <= (sizeof(Professores) / sizeof(Professores[0])) + 1; i++)
+        {
+            if (Professores[i].professor.pis == pis)
+            {
+                achou2 = 1;
+                id_professor = i;
+                break;
+            }
+        }
+        if (achou2 == 1)
+        {
+            Disciplinas[id_disciplina].professordisciplina = Professores[id_professor];
+            printf("\nProfessor da disciplina alterado.\n");
+            return;
+        }
+        else
+        {
+            printf("\nProfessor nao encontrado.\n");
+            return;
+        }
+        
+    }
+    else
+    {
+        printf("\nCodigo de disciplina invalido.\n");
+        return;
+    }
 };
 
 void adicionaAluno(){
@@ -77,6 +122,7 @@ int main(int argc, char const *argv[])
     int controle = 0;
     int qtdDisciplinas = 0;
     int resultado = 0;
+    escola Escola;
 
     strcpy(professores[0].nome, "marcos");
     strcpy(professores[0].sobrenome, "sousa");
@@ -92,6 +138,20 @@ int main(int argc, char const *argv[])
     professores[0].tipo = 1;
     professores[0].professor.pis = 11111;
 
+    strcpy(professores[1].nome, "laura");
+    strcpy(professores[1].sobrenome, "evelyn");
+    professores[1].nacionalidade = 1;
+    professores[1].brasileiro.cpf = 222222222;
+    professores[1].brasileiro.rg = 2222222;
+    professores[1].dia = 20;
+    professores[1].mes = 10;
+    professores[1].ano = 2000;
+    professores[1].pnet = 0;
+    professores[1].cep = 22222222;
+    strcpy(professores[1].endereco, "q300 cj52 cs12");
+    professores[1].tipo = 1;
+    professores[1].professor.pis = 22222;
+
     do
     {
         printf("\nSistema de Disciplinas-------------\n1 - Cadastrar disciplina\n2 - Alterar professor de uma disciplina\n3 - Adicionar um aluno a uma disciplina\n4 - Remover aluno de uma disciplina\n5 - Exibir dados de uma disciplina\n6 - Voltar ao menu principal\n");
@@ -104,6 +164,10 @@ int main(int argc, char const *argv[])
             {
                 qtdDisciplinas++;
             }
+            break;
+
+        case 2:
+            alteraDisciplina(professores, disciplinas);
             break;
 
         case 6:
